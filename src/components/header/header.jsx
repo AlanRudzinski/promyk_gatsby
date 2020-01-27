b0eb98ca7aec10237a0793e430d81f66c1285060
Jordan Russel <rudzinski.a@gmail.com>
2020-01-27 22:46:40 +0100

    header component, site title config change

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,34 +1,52 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
+import styled from 'styled-components';
+
+const NavigationLink = styled.a`
+    text-decoration: none;
+`;
+
+const SiteNavigation = styled.div`
+    width: 60%;
+`;
+
+const NavigationList = styled.ul`
+    list-style-type: none;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const StyledHeader = styled.header`
+    display: flex;
+`;
+
+const Logo = styled.div`
+    width: 40%;
+`;
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+  <StyledHeader>
+    <Logo>
+      <h1>
         <Link
           to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
         >
           {siteTitle}
         </Link>
       </h1>
-    </div>
-  </header>
+    </Logo>
+    <SiteNavigation>
+      <NavigationList>
+        <li><NavigationLink href="/">O nas</NavigationLink></li>
+        <li><NavigationLink href="/">Oferta</NavigationLink></li>
+        <li><NavigationLink href="/">Nasz dzien</NavigationLink></li>
+        <li><NavigationLink href="/">Galeria</NavigationLink></li>
+        <li><NavigationLink href="/">ABC przedszkolaka</NavigationLink></li>
+        <li><NavigationLink href="/">Kontakt</NavigationLink></li>
+      </NavigationList>
+    </SiteNavigation>
+  </StyledHeader>
 );
 
 Header.propTypes = {
