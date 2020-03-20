b78dfd6c75ff24d7c54f60bcd5bcc60cf251e19d
Jordan Russel <rudzinski.a@gmail.com>
2020-03-20 20:59:08 +0100

    broken imgs

--- src/components/HeroImg/HeroImg.jsx
+++ src/components/HeroImg/HeroImg.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
-const HeroImg = () => (
-  <>
-    HeroImg
-  </>
-);
+const HeroImg = () => {
+  const data = useStaticQuery(graphql`
+  query Images {
+    image: file(relativePath: {eq: "heroDuzy.png"}) {
+      id
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid  
+        }
+      }
+    }
+  }
+  `);
+  return (
+    <Img
+      fluid={data.image.childImageSharp.fluid}
+      alt="Krolik plynacy na lodzi"
+    />
+  );
+};
 
 HeroImg.propTypes = {};
 
